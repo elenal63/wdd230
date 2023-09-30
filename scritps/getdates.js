57fd917056a14782f48cdb4ffc188dc4902e5064
elenal63 <91587602+elenal63@users.noreply.github.com>
2023-09-30 09:29:30 -0600

    adjusted css

--- scritps/getdates.js
+++ scritps/getdates.js
@@ -2,4 +2,14 @@ let today1 = new Date();
 document.getElementById("currentyear").textContent=today1.getFullYear();
 document.getElementById("lastmodified").textContent=document.lastModified;
 
+const mainnav = document.querySelector('nav')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+
+
 
