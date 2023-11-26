b699c01a9979cffa9715a88347b912f2543a88b7
elenal63 <91587602+elenal63@users.noreply.github.com>
2023-11-25 21:45:59 -0700

    updated directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,1 +1,67 @@
-const url = "./data/directory.json";
+// Toggle active/not active buttons
+
+var gridSelector = document.querySelector('#directory-grid');
+var listSelector = document.querySelector('#directory-list');
+var directoryData = document.querySelector('#directory-data');
+
+gridSelector.addEventListener('click', ()=>{
+    if (!gridSelector.classList.contains('active')){    
+        gridSelector.classList.add('active');
+        listSelector.classList.remove('active');
+        directoryData.classList.add('directory-cards')
+        directoryData.classList.remove('directory-list')
+    }
+});
+
+listSelector.addEventListener('click', ()=>{
+    if (!listSelector.classList.contains('active')){
+        listSelector.classList.add('active');
+        gridSelector.classList.remove('active');
+        directoryData.classList.add('directory-list')
+        directoryData.classList.remove('directory-cards')
+    }
+});
+
+
+// Load JSON data and do stuff
+const url = "./data/members.json";
+
+const displayMembers = (members) => {
+  const cards = document.querySelector(".directory-cards"); // select the output container element
+
+  members.forEach((business) => {
+    // Create elements to add to the div.cards element
+    let card = document.createElement("section");
+    card.innerHTML = `
+    <img src="${business.imageURL}">
+    <p>${business.name}</p>
+    <p>${business.address}</p>
+    <p>${business.phone}</p>
+    <p>Membership Level: ${business.membershipLevel}</p>
+    <p><a class="card-button" href="${business.websiteURL}">Website</a></p>
+    `;
+    if (business.membershipLevel=='gold'){
+      card.classList.add('gold-member');
+    }
+    cards.appendChild(card);
+  }); // end of forEach loop
+  
+}; // end of function expression
+
+
+
+async function getBusinessData() {
+  const response = await fetch(url);
+  if (response.ok) {
+    const data = await response.json();
+    displayMembers(data.members);
+    // displayBusinessesList(data.members);
+  } else {
+    console.error("There was an error loading the data.");
+    const cards = document.querySelector("directory-cards");
+    cards.innerHTML = "<section><h1>There was an error loading the data</h1></section>";
+  }
+}
+
+getBusinessData();
+
